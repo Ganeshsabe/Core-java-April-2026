186b6e19a15a651626d0e289d83703f1ca9e800e
ganes <ganes@Ganesh_sabe>
2026-06-19 16:04:56 +0530

    Stream api

---
+++ src/com/java/core/streams/Test.java
@@ -0,0 +1,21 @@
+package com.java.core.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Test {
+	public static void main(String[] args) {
+		List<Integer> l = Arrays.asList(12,32,54,32,35,1);
+			
+		for(int a : 1) {
+			System.out.println(a);
+		}
+		
+		System.out.println("--------------------");
+		l.stream().forEach(null);
+
+				
+	}
+
+}
